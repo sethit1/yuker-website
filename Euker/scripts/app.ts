1482d70671ccd665364fe1b5bf93677894e84979
sethit2 <sethrkaplanturner@gmail.com>
2021-12-08 00:30:55 -0600

    planning stage

--- Euker/scripts/app.ts
+++ Euker/scripts/app.ts
@@ -1,1 +1,23 @@
-﻿
+﻿/* theses are notes for how to build yuker program. 
+
+
+1. Build card object, with property suit(1-4) and rank(1-13). Make enums for suits and ranks.  
+2. Build a deck of 52 cards, 4 'suits', 1-13
+3. 'Shuffle' the deck
+4. Create player object, that can hold 1-5 cards, and the players Name
+5. deal the deck, 5 cards to 4 players, and 1 card at dealers side
+6. Ask each player if they want the dealer to pick it up. 
+    5a. If yes, ask the dealer which card he/she would like to lose, and skip to ***7. 
+    5b. If no, ask the first 3 players if they want to choose suit 1, 2, 3, 4, or pass. 
+        5b1. If yes, set suit and skip to ***7. 
+        5b2. If no, ask dealer if he/she wants suit 1, 2, 3, 4, or no trump. 
+7. 
+
+
+
+
+
+
+
+
+*/
